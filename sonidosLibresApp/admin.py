9b9dffdbbc958b665c77dbebd79bf5d06b56f9ae
Fernanda Calderón Pérez <fercalderon.406@gmail.com>
2016-10-22 17:31:56 -0500

    Creación Modelo Convocation, ConvocationAudio, ConvocationVoting

--- sonidosLibresApp/admin.py
+++ sonidosLibresApp/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Category, Audio, Commentary, Album, Artist
+from .models import Category, Audio, Commentary, Album, Artist,Convocation,ConvocationAudio,ConvocationVoting,Agent
 
 admin.site.register(Category)
 admin.site.register(Audio)
 admin.site.register(Commentary)
 admin.site.register(Album)
 admin.site.register(Artist)
+admin.site.register(ConvocationVoting)
+admin.site.register(Convocation)
+admin.site.register(ConvocationAudio)
+admin.site.register(Agent)
--- sonidosLibresApp/models.py
+++ sonidosLibresApp/models.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 
 import django
 from django.db import models
@@ -6,6 +5,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
+
 
 class Category(models.Model):
     def __str__(self):
@@ -70,9 +70,9 @@ class Commentary (models.Model):
     class Meta:
         verbose_name_plural = "commentaries"
     commentary = models.TextField()
-    date = models.DateTimeField(editable=False, default = django.utils.timezone.now)
+    date = models.DateTimeField(editable=False, default=django.utils.timezone.now)
     audio = models.ForeignKey(Audio,on_delete=models.CASCADE)
-    user = models.OneToOneField(User, null=True, blank=True
+    user = models.OneToOneField(User, null=True, blank=True)
 
 class Agent(models.Model):
     def __str__(self):
@@ -94,7 +94,37 @@ class Agent(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.artist.save()
 
+class Convocation(models.Model):
+    def __str__(self):
+        return self.title + " " + str(self.id)
+    public='PUB'
+    private='PRIV'
+    unpublished='UP'
+    published='P'
+    vote='V'
+    close='C'
 
+    name = models.CharField(max_length=40)
+    detail = models.CharField(max_length=5000)
+    agent = models.ForeignKey(Agent,on_delete=models.CASCADE)
+    typeConvocation = ((public,'Convocatoria Publica'),(private,'Convocatoria Privada'))
+    terms = models.URLField()
+    dateInit = models.DateTimeField(editable=True, default=django.utils.timezone.now)
+    dateEnd = models.DateTimeField(editable=True, default=django.utils.timezone.now)
+    dateLimit = models.DateTimeField(editable=True, default=django.utils.timezone.now)
+    dateResults = models.DateTimeField(editable=True, default=django.utils.timezone.now)
+    status = ((unpublished,'Sin Publicar'),(published,'Publicada'),(vote,'En Votacion'),(close,'Cerrada'))
+    winner = models.ForeignKey(Artist,on_delete=models.CASCADE,null=True,blank=True)
+
+class ConvocationAudio(models.Model):
+    convocation=models.ForeignKey(Convocation,on_delete=models.CASCADE)
+    audio=models.ForeignKey(Audio,on_delete=models.CASCADE)
+    votes=models.IntegerField(editable=False, default = 0)
+
+
+class ConvocationVoting(models.Model):
+    convocation=models.ForeignKey(Convocation,on_delete=models.CASCADE)
+    artist=models.ForeignKey(Artist,on_delete=models.CASCADE)
 
 
     # python manage.py makemigrations sonidosLibresApp

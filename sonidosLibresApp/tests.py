adebe88942bdb0091a8073710e247818d50c4b40
John <jfmolanob@uniandes.edu.co>
2016-10-31 22:47:37 -0500

    Tests - RateAudio, RateAlbum, Play, Download

--- sonidosLibresApp/tests.py
+++ sonidosLibresApp/tests.py
@@ -256,3 +256,245 @@ class CommentaryTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class AudioAlbumAssociationTest(APITestCase):
+
+    def testAudioAlbumAssociation(self):
+        url = '/api/categories/'
+        data = {'name': 'categoriaDemo',
+                'image': 'https://www.google.com.co',
+                'description': 'Description'
+                }
+        self.client.post(url, data, format='json')
+        category = Category.objects.get(name='categoriaDemo')
+
+        url = '/api/artists/'
+        data = {'name': 'artistName',
+                'image': 'https://www.google.com.co'
+                }
+        self.client.post(url, data, format='json')
+        artist = Artist.objects.get(name='artistName')
+
+        url = '/api/albums/'
+        data = {
+            'title': 'albumTitle',
+            'image': 'http://loudwire.com/files/2015/09/Nevermind.jpg',
+            'categories': [str(category.id)],
+            'artists': [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        album = Album.objects.get(title='albumTitle')
+
+        url = '/api/audios/'
+        data = {
+            "name": "d8d1b374-0fa4-4daf-ac9b-3e506662d78d",
+            "title": "AudioDemo",
+            "audioDownload": "http://www.dropbox.com",
+            "audioPlay": "http://www.dropbox.com",
+            "categories": [str(category.id)],
+            "albums": [str(album.id)],
+            "artists": [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        audio = Audio.objects.get(name='d8d1b374-0fa4-4daf-ac9b-3e506662d78d')
+
+        url = '/api/albumAudio/' + str(audio.id) + "/" + str(album.id) + "/"
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class RateAudioTest(APITestCase):
+
+    def testRateAudio(self):
+        url = '/api/categories/'
+        data = {'name': 'categoriaDemo',
+                'image': 'https://www.google.com.co',
+                'description': 'Description'
+                }
+        self.client.post(url, data, format='json')
+        category = Category.objects.get(name='categoriaDemo')
+
+        url = '/api/artists/'
+        data = {'name': 'artistName',
+                'image': 'https://www.google.com.co'
+                }
+        self.client.post(url, data, format='json')
+        artist = Artist.objects.get(name='artistName')
+
+        url = '/api/albums/'
+        data = {
+            'title': 'albumTitle',
+            'image': 'http://loudwire.com/files/2015/09/Nevermind.jpg',
+            'categories': [str(category.id)],
+            'artists': [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        album = Album.objects.get(title='albumTitle')
+
+        url = '/api/audios/'
+        data = {
+            "name": "d8d1b374-0fa4-4daf-ac9b-3e506662d78d",
+            "title": "AudioDemo",
+            "audioDownload": "http://www.dropbox.com",
+            "audioPlay": "http://www.dropbox.com",
+            "categories": [str(category.id)],
+            "albums": [str(album.id)],
+            "artists": [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        audio = Audio.objects.get(name='d8d1b374-0fa4-4daf-ac9b-3e506662d78d')
+
+        i = 0
+        while i <= 5:
+            url = '/api/rateAudio/' + str(audio.id) + "/" + str(i) + "/"
+            response = self.client.get(url, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            i += 1
+        while i <= 10:
+            url = '/api/rateAudio/' + str(audio.id) + "/" + str(i) + "/"
+            response = self.client.get(url, format='json')
+            self.assertNotEquals(response.status_code, status.HTTP_200_OK)
+            i += 1
+
+
+class RateAlbumTest(APITestCase):
+
+    def testRateAlbum(self):
+        url = '/api/categories/'
+        data = {'name': 'categoriaDemo',
+                'image': 'https://www.google.com.co',
+                'description': 'Description'
+                }
+        self.client.post(url, data, format='json')
+        category = Category.objects.get(name='categoriaDemo')
+
+        url = '/api/artists/'
+        data = {'name': 'artistName',
+                'image': 'https://www.google.com.co'
+                }
+        self.client.post(url, data, format='json')
+        artist = Artist.objects.get(name='artistName')
+
+        url = '/api/albums/'
+        data = {
+            'title': 'albumTitle',
+            'image': 'http://loudwire.com/files/2015/09/Nevermind.jpg',
+            'categories': [str(category.id)],
+            'artists': [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        album = Album.objects.get(title='albumTitle')
+
+        i = 0
+        while i <= 5:
+            url = '/api/rateAlbum/' + str(album.id) + "/" + str(i) + "/"
+            response = self.client.get(url, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            i += 1
+        while i <= 10:
+            url = '/api/rateAlbum/' + str(album.id) + "/" + str(i) + "/"
+            response = self.client.get(url, format='json')
+            self.assertNotEquals(response.status_code, status.HTTP_200_OK)
+            i += 1
+
+
+class PlayAudioTest(APITestCase):
+
+    def testPlayAudio(self):
+        url = '/api/categories/'
+        data = {'name': 'categoriaDemo',
+                'image': 'https://www.google.com.co',
+                'description': 'Description'
+                }
+        self.client.post(url, data, format='json')
+        category = Category.objects.get(name='categoriaDemo')
+
+        url = '/api/artists/'
+        data = {'name': 'artistName',
+                'image': 'https://www.google.com.co'
+                }
+        self.client.post(url, data, format='json')
+        artist = Artist.objects.get(name='artistName')
+
+        url = '/api/albums/'
+        data = {
+            'title': 'albumTitle',
+            'image': 'http://loudwire.com/files/2015/09/Nevermind.jpg',
+            'categories': [str(category.id)],
+            'artists': [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        album = Album.objects.get(title='albumTitle')
+
+        url = '/api/audios/'
+        data = {
+            "name": "d8d1b374-0fa4-4daf-ac9b-3e506662d78d",
+            "title": "AudioDemo",
+            "audioDownload": "http://www.dropbox.com",
+            "audioPlay": "http://www.dropbox.com",
+            "categories": [str(category.id)],
+            "albums": [str(album.id)],
+            "artists": [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        audio = Audio.objects.get(name='d8d1b374-0fa4-4daf-ac9b-3e506662d78d')
+
+        i = audio.playCount
+        url = '/api/play/' + str(audio.id)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        audio = Audio.objects.get(id=audio.id)
+        self.assertEqual(audio.playCount, i + 1)
+
+
+class DownloadAudioTest(APITestCase):
+
+    def testDownloadAudio(self):
+        url = '/api/categories/'
+        data = {'name': 'categoriaDemo',
+                'image': 'https://www.google.com.co',
+                'description': 'Description'
+                }
+        self.client.post(url, data, format='json')
+        category = Category.objects.get(name='categoriaDemo')
+
+        url = '/api/artists/'
+        data = {'name': 'artistName',
+                'image': 'https://www.google.com.co'
+                }
+        self.client.post(url, data, format='json')
+        artist = Artist.objects.get(name='artistName')
+
+        url = '/api/albums/'
+        data = {
+            'title': 'albumTitle',
+            'image': 'http://loudwire.com/files/2015/09/Nevermind.jpg',
+            'categories': [str(category.id)],
+            'artists': [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        album = Album.objects.get(title='albumTitle')
+
+        url = '/api/audios/'
+        data = {
+            "name": "d8d1b374-0fa4-4daf-ac9b-3e506662d78d",
+            "title": "AudioDemo",
+            "audioDownload": "http://www.dropbox.com",
+            "audioPlay": "http://www.dropbox.com",
+            "categories": [str(category.id)],
+            "albums": [str(album.id)],
+            "artists": [str(artist.id)]
+        }
+        self.client.post(url, data, format='json')
+        audio = Audio.objects.get(name='d8d1b374-0fa4-4daf-ac9b-3e506662d78d')
+
+        i = audio.downloadsCount
+        url = '/api/download/' + str(audio.id)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        audio = Audio.objects.get(id=audio.id)
+        self.assertEqual(audio.downloadsCount, i + 1)
